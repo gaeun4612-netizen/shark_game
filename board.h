6c8b3a5cea7507d578bd35ae31534fc8b8532917
gaeun4612-netizen <gaeun4612@gmail.com>
2025-11-19 16:04:52 +0900

    02

--- board.h
+++ board.h
@@ -1,16 +1,4 @@
-void board_printBoardStatus(void)
-{
-	int i;
-	
-	printf("------------BOARD STATUS------------");
-	for(i=0;i<N_BOARD;i++)
-	{
-		printf("|");
-		if (board_status[i] == BOARDSTATUS_NOK)
-			printf("X");
-		else
-			printf("O");
-	}
-	printf("|\n");
-	printf("------------------------------------");
-}
+void board_printBoardStatus(void);
+int board_getBoardStatus(int pos);
+int board_getBoardCoin(int pos);
+void board_initBoard(void);
--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "board.h"
+
+#define MAX_DIE   6
+
+int rolldie(void)
+{
+	return rand()%MAX_DIE+1;
+}
 
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int cnt;
+	int pos;
+	int coinResult;
 	
 	srand((unsigned)time(NULL));
 	
@@ -17,12 +29,34 @@ int main(int argc, char *argv[]) {
 	printf("--<@_###############_@>--\n");
 	
 	//step 1. initialization (playername setting, variables)
+	board_initBoard();
 	
 	//step 2. turn play (do-while)
+	cnt=0;
+	pos=0;
+	coinResult=0;
+	do {
+		int die_result;
+		
 	//     2-1. status printing
+	board_printBoardStatus ();
+	
 	//     2-2. rojll die
+	die_result = rolldie();
+	
 	//     2-3. move (result)
+	pos+= die_result;
+	
+	printf("pos : %i (die:%i)\n", pos, die_result);
+	
+	coinResult += board_getBoardCoin(pos);
+	
+	printf("coin : %i\n",coinResult);
+	
 	//     2-4. change turn, shark move
+	cnt++;
+	
+	} while(cnt < 5);
 	
 	//step 3. game end(winner printing)
 	
